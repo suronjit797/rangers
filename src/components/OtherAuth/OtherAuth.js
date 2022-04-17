1bef301a638199d5aa7f0b506f2da2d46b1c2e24
Suronjit pal <suronjit797@gmail.com>
2022-04-17 20:35:10 +0600

    error message

--- src/components/OtherAuth/OtherAuth.js
+++ src/components/OtherAuth/OtherAuth.js
@@ -10,34 +10,37 @@ import facebook from '../../images/facebook.png'
 import auth from '../../firebase.init';
 import { useNavigate } from 'react-router-dom';
 import { Spinner } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
 
 
 const OtherAuth = () => {
     const navigate = useNavigate()
-    const [error, setError] = useState('')
+
+    // react tost
+    const notify = err => toast(err);
 
     // firebase hooks
-    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth, {sendEmailVerification: true});
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth, { sendEmailVerification: true });
     const [signInWithFacebook, fbUser, fbLoading, fbError] = useSignInWithFacebook(auth);
 
     useEffect(() => {
         if (googleError) {
-            setError(FirebaseErrorMessage(googleError.message))
+            notify(FirebaseErrorMessage(googleError.message))
         }
         if (fbError) {
-            setError(FirebaseErrorMessage(fbError.message))
+            notify(FirebaseErrorMessage(fbError.message))
         }
     }, [googleError, fbError])
 
     if (googleLoading || fbLoading) {
         return (
-            <div className='spinner_body' >
+            <div className="d-block mx-auto">
                 <Spinner animation="grow" variant="warning" />
             </div>
         );
     }
 
-    if(googleUser || fbUser){
+    if (googleUser || fbUser) {
         navigate('/')
     }
 
@@ -54,7 +57,6 @@ const OtherAuth = () => {
     return (
         <div className='otherAuth' >
             <div className="divider"> <span>or</span> </div>
-            <p className="text-danger text-center"> {error} </p>
             <div className="mt-3">
                 <button className="btn btn btn-outline-info" onClick={handelGoogle}>
                     <img src={google} alt="" />
